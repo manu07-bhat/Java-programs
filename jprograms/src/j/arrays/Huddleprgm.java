71e62a1751267f33c4ec91531497c37345051432
Manoj <Manubhat701@gmail.com>
2023-07-22 21:45:53 +0530

    MAX | MIN Value in an array | Object calling | Static calling

--- jprograms/src/j/arrays/Huddleprgm.java
+++ jprograms/src/j/arrays/Huddleprgm.java
@@ -1,23 +1,47 @@
 package j.arrays;
 
 public class Huddleprgm {
-
-	public static void main(String[] args) {
-		// Hacker Rank question
+	// Hacker Rank question
+	static void MAX()
+	{
 		//Max of an aray
 		
-		int x[] = {0,2,1,5,10,5};
+				int x[] = {0,2,1,5,10,5};
+				
+				int max =0;
+				
+				for (int i = 0; i < x.length; i++) {
+					
+					if(max < x[i])
+					{
+						max = x[i];
+					}
+				}
+				System.out.println("MAX IS "+max);
+				
+	}
+	
+	void MIN()
+	{
+		int y[] = {0,-2,1,5,10,-5};
 		
-		int max =0;
+		int min =0;
 		
-		for (int i = 0; i < x.length; i++) {
+		for (int i = 0; i < y.length; i++) {
 			
-			if(max < x[i])
+			if(min > y[i])
 			{
-				max = x[i];
+				min = y[i];
 			}
+			
 		}
-		System.out.println("MAX IS "+max);
+		
+		System.out.println(min);
+	}
+	public static void main(String[] args) {
+		Huddleprgm obj = new Huddleprgm();
+		obj.MIN();
+		MAX(); //static calling
 		
 		
 	}
