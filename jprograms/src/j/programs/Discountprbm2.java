3a1f827620fe494fcad16edb2ab350e5ae16230d
Manoj <Manubhat701@gmail.com>
2023-07-24 20:51:46 +0530

    small changes

--- jprograms/src/j/programs/Discountprbm2.java
+++ jprograms/src/j/programs/Discountprbm2.java
@@ -21,24 +21,25 @@ public class Discountprbm2 {
 		if(total_amt>25_000 && total_amt<=50_000)
 		{
 			whole_dis = total_amt *5/100;
-			System.out.print("whole_product Discount ");
-			return whole_dis;
+			System.out.print("whole_product Discount "+whole_dis);
+			
 		}
 		else if(total_amt>50_000 && total_amt<=10_00_000)
 		{
 			whole_dis = total_amt *7/100;
-			System.out.print("whole_product Discount ");
-			return whole_dis;
+			System.out.print("whole_product Discount "+whole_dis);
+			
 		}
 		else if(total_amt>10_00_000 )
 		{
 			whole_dis = total_amt *10/100;
-			System.out.print("whole_product Discount ");
-			return whole_dis;
+			System.out.print("whole_product Discount "+whole_dis);
+			
 		}
 		
 		System.out.println();
 		System.err.println("| RECOMDATION |");
+		
 		if(totaldis_per_product == whole_dis)
 		{
 			System.out.println("Choose any Discount bec both are having same discount");
@@ -46,13 +47,13 @@ public class Discountprbm2 {
 		}
 		else if(totaldis_per_product > whole_dis )
 		{
-			System.out.print("Take the per_product Discount "+ (totaldis_per_product - whole_dis)+ " Is more discount & discount " );
+			System.out.print("Take the per_product Discount "+ (totaldis_per_product - whole_dis)+ " Is more discount & discount "+totaldis_per_product );
 			return   totaldis_per_product;
 		}
 		else
 		{
-			System.out.print("Take the whole_product Discount "+ (whole_dis- totaldis_per_product)+ " Is more discount & discount ");
-			return    whole_dis;
+			System.out.print("Take the whole_product Discount "+ (whole_dis- totaldis_per_product)+ " Is more discount & discount "+whole_dis);
+			return  whole_dis;
 		}
 		
 		
