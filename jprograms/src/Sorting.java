482a625344b61155011a20308120eaf1959fb91a
Manoj <Manubhat701@gmail.com>
2023-07-28 13:31:02 +0530

    Selection sort

--- jprograms/src/Sorting.java
+++ jprograms/src/Sorting.java
@@ -31,6 +31,25 @@ public class Sorting {
 		}
 	}
 
+	static void selectionSort(int ar[])
+	{
+		for (int i = 0; i < ar.length-1; i++) {
+			
+			int min_i =i;
+			
+			for (int j = i+1; j < ar.length; j++) {
+				
+				if(ar[j] < ar[min_i])
+				{
+					min_i =j;
+				}
+			}
+			int temp = ar[i];
+			ar[i] = ar[min_i];
+			ar[min_i] =temp;
+		}
+	}
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
@@ -40,7 +59,7 @@ public class Sorting {
 		for (int i = 0; i < ar.length; i++) {
 			ar[i] = scan.nextInt();
 		}
-		insertionSort(ar);
+		selectionSort(ar);
 		for (int i = 0; i < ar.length; i++) {
 			System.out.print(ar[i] + " ");
 		}
