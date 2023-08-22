d91d4072bef603f72dacf76bd1718c7f1769bff0
Manoj <Manubhat701@gmail.com>
2023-08-22 20:06:44 +0530

    MultiThread from single program

--- jprograms/src/j/threads/ThreadsProgram1.java
+++ jprograms/src/j/threads/ThreadsProgram1.java
@@ -2,9 +2,9 @@ package j.threads;
 
 import java.util.Scanner;
 
-public class ThreadsProgram1 {
+class Test1 extends Thread {
 
-	static void add() {
+	public void run() {
 		System.out.println("Addition started ");
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the first number: ");
@@ -15,34 +15,61 @@ public class ThreadsProgram1 {
 		int c = a + b;
 		System.out.println(c);
 		System.out.println("Addition is ended");
-
 	}
+}
 
-	static void printChar() throws Exception {
+class Test2 extends Thread {
+
+	public void run() {
 		System.out.println("Printing char started");
 		for (int i = 65; i <= 75; i++) {
-			System.out.println((char) i);
-			Thread.sleep(4000);
+			System.out.println("  " + (char) i);
+			try {
+				Thread.sleep(4000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 		System.out.println("Printing char ended");
 
 	}
 
-	static void printNum() throws Exception {
+}
+
+class Test3 extends Thread {
+
+	public void run() {
 		System.out.println("Printing number started");
 
 		for (int i = 10; i <= 20; i++) {
-			System.out.println(i);
-			Thread.sleep(4000);
+			System.out.println("    " + i);
+			try {
+				Thread.sleep(4000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		System.out.println("Printing number ended");
+
 	}
+}
+
+public class ThreadsProgram1 {
 
 	public static void main(String[] args) throws Exception {
-		add();
-		printChar();
-		printNum();
+
+		Thread t = Thread.currentThread();
+		System.out.println(t);
+
+		Test1 t1 = new Test1();
+		Test2 t2 = new Test2();
+		Test3 t3 = new Test3();
+
+		t1.start();
+		t2.start();
+		t3.start();
 
 	}
 
